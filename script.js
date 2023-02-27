d755ee5cf9a4ef5d2d48f696c662828d6381d9ff
mohit <54810032+mohitgupta-16@users.noreply.github.com>
2023-02-27 14:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+const images = document.querySelectorAll('.image');
+
+let draggedImage = null;
+
+images.forEach(image => {
+  image.addEventListener('dragstart', () => {
+    draggedImage = image;
+  });
+
+  image.addEventListener('dragover', e => {
+    e.preventDefault();
+  });
+
+  image.addEventListener('drop', () => {
+    if (draggedImage !== image) {
+      const temp = image.innerHTML;
+      image.innerHTML = draggedImage.innerHTML;
+      draggedImage.innerHTML = temp;
+    }
+  });
+});
